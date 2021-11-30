2429e7bf740312e3726f9e4d15376dbd2b5ca800
salma-anika <anikaavaa@gmail.com>
2021-12-01 01:39:48 +0600

    Price page has  been set

--- src/Price.js
+++ src/Price.js
@@ -1,29 +1,48 @@
 function Clients() {
     return (
-      <div className="clients" >
+      <div className="price" >
         
-        <div className="clients_container">
+        <div className="Price_container">
        <div className="row">
-           <div className="col-3">
+           <div className="col-4">
+           <div className="prices">
+           <div className="price_img">
            <h1 className="clients_h1"> 120</h1>
    <p className="clients_p">Awesome Employees </p>
   
    </div>
-   <div className="col-3">
+   </div>
+   </div>
+   <div className="col-4">
+           <div className="prices">
+           <div className="price_img">
            <h1 className="clients_h1"> 58</h1>
    <p className="clients_p">Pizza Types </p>
   
    </div>
-   <div className="col-3">
+   </div>
+   </div>
+   <div className="col-4">
+           <div className="prices">
+           <div className="price_img">
            <h1 className="clients_h1"> 1464</h1>
    <p className="clients_p">Satisfied Clients </p>
   
    </div>
-   <div className="col-3">
-           <h1 className="clients_h1"> 2031</h1>
-   <p className="clients_p">Total Delivery </p>
+   </div>
+   </div>
+   <div className="col-4">
+           <div className="prices">
+           <div className="price_img">
+               <img src="img/1.png"/>
+           <h1 className="price_h1"> Pizza Margaritta</h1>
+   <p className="price_p">Lorem ipsum dolor sit amet, <br/> consectetur adipiscing elit, <br/>sed do eiusmod tempor incididunt ut <br/>labore et dolore magna aliqua.<br/> </p>
+   <p className="price_p">$50.00 </p>
   
    </div>
+   </div>
+   </div>
+
    </div>
        </div>
       </div>
