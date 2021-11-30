77c9d9fab6ad7e8fccfbed55486df346b6b0e04b
salma-anika <anikaavaa@gmail.com>
2021-12-01 03:06:39 +0600

    Footer page has been created

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Shop from './Shop';
 import View from './View';
 import Clients from './Clients';
 import Price from './Price';
+import Footer from './Footer';
 
 import SignUp from './SignUp';
 import logo from './logo.svg';
@@ -23,6 +24,7 @@ function App() {
       <View/>
       <Clients/>
       <Price/>
+      <Footer/>
       
      
       </BrowserRouter>
---
+++ src/Footer.js
@@ -0,0 +1,48 @@
+function Footer() {
+    return (
+      <div className="price" >
+        
+        <div className="Price_container">
+       <div className="row">
+           <div className="col-4">
+           <div className="prices">
+           <div className="price_img">
+               <img className="prices_img" src="img/1.png"/>
+           <h1 className="price_h1"> Pizza Margaritta</h1>
+   <p className="price_p">Lorem ipsum dolor sit amet, <br/> consectetur adipiscing elit, <br/>sed do eiusmod tempor incididunt ut <br/>labore et dolore magna aliqua.<br/> </p>
+   <p className="price_p">$50.00 </p>
+  
+   </div>
+   </div>
+   </div>
+   <div className="col-4">
+   <div className="prices">
+           <div className="price_img">
+               <img className="prices_img" src="img/e.jpg"/>
+           <h1 className="price_h1"> Pizza Margaritta</h1>
+   <p className="price_p">Lorem ipsum dolor sit amet, <br/> consectetur adipiscing elit, <br/>sed do eiusmod tempor incididunt ut <br/>labore et dolore magna aliqua.<br/> </p>
+   <p className="price_p">$50.00 </p>
+  
+   </div>
+   </div>
+   </div>
+   <div className="col-4">
+   <div className="prices">
+           <div className="price_img">
+               <img className="prices_img" src="img/f.jpg"/>
+           <h1 className="price_h1"> Pizza Margaritta</h1>
+   <p className="price_p">Lorem ipsum dolor sit amet, <br/> consectetur adipiscing elit, <br/>sed do eiusmod tempor incididunt ut <br/>labore et dolore magna aliqua.<br/> </p>
+   <p className="price_p">$50.00 </p>
+  
+   </div>
+   </div>
+   </div>
+  
+   
+   </div>
+       </div>
+      </div>
+    );
+  }
+  
+  export default Footer;
