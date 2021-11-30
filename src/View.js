7304504ccad948ba010e43de470f4c7285a2b480
salma-anika <anikaavaa@gmail.com>
2021-11-30 22:44:45 +0600

    View header and text has been changed and designed

--- src/View.js
+++ src/View.js
@@ -2,7 +2,7 @@ function View() {
     return (
       <div >
         
-        <div className="ab_container">
+        <div className="view_container">
        <div className="row">
            <div className="col-6">
            <div className="ab_image">
@@ -11,9 +11,9 @@ function View() {
   
    </div>
    <div className="col-6">
-   <h3 className="about_h3">The Pizza Menu</h3>
-   <h1 className="about_h1"> CHICAGO <br/>THIN CRUST</h1>
-   <p className="about_p">Lorem ipsum dolor sit amet, <br/> consectetur adipiscing elit, <br/>sed do eiusmod tempor incididunt ut <br/>labore et dolore magna aliqua.<br/> </p>
+   <h3 className="view_h3">The Pizza Menu</h3>
+   <h1 className="view_h1"> CHICAGO <br/>THIN CRUST</h1>
+   <p className="view_p">Lorem ipsum dolor sit amet, <br/> consectetur adipiscing elit, <br/>sed do eiusmod tempor incididunt ut <br/>labore et dolore magna aliqua.<br/> </p>
    <br/>
    <button className="btn btn-lg btn-danger">VIEW MORE</button>
        </div>
