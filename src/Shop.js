3414975f1e0e2fea98ca26c067f67d738f10893b
salma-anika <anikaavaa@gmail.com>
2021-11-30 22:19:19 +0600

    Shop image has been set

---
+++ src/Shop.js
@@ -0,0 +1,22 @@
+
+
+function Shop() {
+    return (
+      <div >
+        
+      <img class ="shop-img" src="img/d.jpg"/>
+ <div className="shop">
+  
+   <div className="shop_container">
+   <h3 className="shop_h3">Free Delivery With  <br/> PIZZA OF THE DAY  <br/>ONLY 9.99 USD</h3>
+  
+  
+   <br/>
+   <button className="btn btn-lg btn-danger">SHOP NOW</button>
+   </div>
+       </div>
+      </div>
+    );
+  }
+  
+  export default Shop;
