8c851a675f8c306c05ca07089e612d8f2c286ad1
salma-anika <anikaavaa@gmail.com>
2021-11-30 22:30:31 +0600

    View page has been changed

--- src/View.js
+++ src/View.js
@@ -2,15 +2,21 @@ function View() {
     return (
       <div >
         
-      <img class ="shop-img" src="img/d.jpg"/>
- <div className="shop">
+        <div className="ab_container">
+       <div className="row">
+           <div className="col-6">
+           <div className="ab_image">
+           <img src="img/c.jpg"/>
+       </div>
   
-   <div className="shop_container">
-   <h3 className="shop_h3">Free Delivery With <br/>PIZZA OF THE DAY  <br/>ONLY 9.99 USD</h3>
-  
-  
+   </div>
+   <div className="col-6">
+   <h3 className="about_h3">The Pizza Menu</h3>
+   <h1 className="about_h1"> CHICAGO <br/>THIN CRUST</h1>
+   <p className="about_p">Lorem ipsum dolor sit amet, <br/> consectetur adipiscing elit, <br/>sed do eiusmod tempor incididunt ut <br/>labore et dolore magna aliqua.<br/> </p>
    <br/>
    <button className="btn btn-lg btn-danger">VIEW MORE</button>
+       </div>
    </div>
        </div>
       </div>
