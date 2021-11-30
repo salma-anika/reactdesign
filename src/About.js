306fb890b46199511798db5350157436d96e90c7
salma-anika <anikaavaa@gmail.com>
2021-11-30 20:56:33 +0600

    create about page

--- src/About.js
+++ src/About.js
@@ -1,22 +1,20 @@
 import Navs from './Navs';
 
-function Header() {
+function About() {
     return (
       <div >
-        <Navs/>
-      <img class ="head-img" src="img/b.JPG"/>
- <div className="head-content">
-   <Navs/>
+ <div className="about">
+  
    <div className="container">
-   <h3>Pizza Delivery</h3>
-   <h1>MAESTRO <br/> PIZZINNI</h1>
+   <h3>About Us</h3>
+   <h1> WELCOME TO MAESTRO <br/> PIZZINNI</h1>
    <p>Lorem ipsum dolor sit amet, <br/> consectetur adipiscing elit, <br/>sed do eiusmod tempor incididunt ut <br/>labore et dolore magna aliqua.<br/> </p>
    <br/>
-   <button className="btn btn-lg btn-danger">Delivery Now</button>
+   <button className="btn btn-lg btn-danger">READ MORE</button>
    </div>
        </div>
       </div>
     );
   }
   
-  export default Header;
+  export default About;
--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
+import About from './About';
 
 import SignUp from './SignUp';
 import logo from './logo.svg';
@@ -13,6 +14,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Header/>
+      <About/>
       
      
       </BrowserRouter>
