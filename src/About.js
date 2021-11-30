70f04c06180a71e19fd8d8bb33da6a03b2d78f2c
salma-anika <anikaavaa@gmail.com>
2021-11-30 21:23:24 +0600

    create about image and add some text

--- src/About.js
+++ src/About.js
@@ -2,15 +2,23 @@ import Navs from './Navs';
 
 function About() {
     return (
-      <div >
+     
  <div className="about">
   
-   <div className="container">
+   <div className="ab_container">
+       <div className="row">
+           <div className="col-6">
    <h3>About Us</h3>
    <h1> WELCOME TO MAESTRO <br/> PIZZINNI</h1>
    <p>Lorem ipsum dolor sit amet, <br/> consectetur adipiscing elit, <br/>sed do eiusmod tempor incididunt ut <br/>labore et dolore magna aliqua.<br/> </p>
    <br/>
    <button className="btn btn-lg btn-danger">READ MORE</button>
+   </div>
+   <div className="col-6">
+       <div className="ab_image">
+           <img src="img/c.jpg"/>
+       </div>
+       </div>
    </div>
        </div>
       </div>
